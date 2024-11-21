2c74375e2e8ef0e7124c781322e3499e526bae17
Mathias Conradt <mathias.conradt@protonmail.com>
2024-11-21 16:31:23 +0100

    fix sqli

--- src/main/java/org/workshop/coffee/controller/UploadController.java
+++ src/main/java/org/workshop/coffee/controller/UploadController.java
@@ -32,9 +32,15 @@ public class UploadController {
     @PostMapping("/uploadimage")
     public String uploadImage(Model model, @RequestParam("image") MultipartFile file, Principal principal) throws IOException {
 
-//        model.addAttribute("msg", "Uploaded images: " + name);
-//        getPerson(model, principal).setProfilePic(name);
-//        personService.savePerson(getPerson(model, principal));
+        // Get file and save it in the UPLOAD_DIRECTORY
+        var name = file.getOriginalFilename().replace(" ", "_");
+        var fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, name);
+        Files.write(fileNameAndPath, file.getBytes());
+
+
+        model.addAttribute("msg", "Uploaded images: " + name);
+        getPerson(model, principal).setProfilePic(name);
+        personService.savePerson(getPerson(model, principal));
 
         return "person/upload";
     }
